3e2155fc171141423612656475e0d5741b1aebdc
TwistRU <pavlyuk.rs@students.dvfu.ru>
2021-04-22 20:38:54 +1000

    Добавил классы и EventListener'ы

--- script.js
+++ script.js
@@ -1,9 +1,31 @@
 'use strict';
 
+class Player {
+    constructor() {
+        this.countOfLives = 3;
+    }
+}
+
+class Obstacle {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+}
+
+class Enemy {
+
+}
+
+class Bullet {
+
+}
+
 class Game {
     constructor(ctx) {
         this.alive = true;
         this.arrayOfStars = [];
+        this.arrayOfObstacles = [];
         this.ctx = ctx;
         this.width = window.innerWidth;
         this.height = window.innerHeight;
@@ -35,7 +57,6 @@ class Game {
         }
     }
 
-
     prepareInGameBackground() {
         for (let i = 0; i < Math.floor(this.width * this.height / 2500); i++) {
             this.arrayOfStars.push({x: Math.random() * this.width, y: Math.random() * this.height})
@@ -45,28 +66,49 @@ class Game {
 }
 
 window.onload = function () {
-    document.getElementById('myCanvas').onmousemove = changeMousePos;
-    setInterval(draw, 1);
+    document.addEventListener("mousemove", changeMousePos, true);
+    document.addEventListener('keydown', keyPressed, true);
+    document.addEventListener('keyup', keyReleased, true);
 }
 
 let canvas = document.getElementById('myCanvas');
-canvas.setAttribute('width', (window.innerWidth * 0.9).toString());
-canvas.setAttribute('height', (window.innerHeight * 0.9).toString());
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
 const ctx = canvas.getContext('2d');
+const KEY_CODES = {
+    LEFT_KEY: 37,
+    UP_KEY: 38,
+    RIGHT_KEY: 39,
+    DOWN_KEY: 40,
+}
+let keysStatus = {};
 let mouseX = 0;
 let mouseY = 0;
 let game = new Game(ctx);
 let inGame = true;
+let drawInterval = setInterval(draw, 1000 / 60);
 
 function draw() {
+    // drawInterval
     if (inGame) {
         inGame = game.draw();
     }
 }
 
 function changeMousePos(e) {
+    // EventListener mousemove
     mouseY = e.clientY;
     mouseX = e.clientX;
+}
+
+function keyPressed(e) {
+    // EventListener keydown
+    keysStatus[e.keyCode] = true;
+}
+
+function keyReleased(e) {
+    // EventListener keyup
+    keysStatus[e.keyCode] = false;
 }
 
 
